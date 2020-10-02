85c7a5b20e9a60a5378da7f5a5f8198566615356
Calvin Cattaneo <calvin.cattaneo@gmail.com>
2020-10-02 16:14:29 -0300

    project

---
+++ Assets/Scripts/ColisorTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisorTrigger : MonoBehaviour {
+    void OnTriggerEnter(Collider colisor) {
+        if(colisor.gameObject.tag == "Player") {
+            Debug.Log("Tocou!");
+        }
+    }
+
+}
